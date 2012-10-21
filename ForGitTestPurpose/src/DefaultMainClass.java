3040be0433dffae3308246aa3d9a8d430426ba87
quintescent <mr.quintescent@gmail.com>
2012-10-21 17:22:22 +0200

    Just testing

---
+++ ForGitTestPurpose/src/DefaultMainClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author quintescent
+ *
+ */
+public class DefaultMainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
